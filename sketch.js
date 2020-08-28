efdc4967bb5b08520330a15b3c32c053ff34d531
Ace369 <aditi.chamarthy@gmail.com>
2020-08-28 16:52:28 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -55,18 +55,14 @@ function draw() {
   packageSprite.x= packageBody.position.x 
   packageSprite.y= packageBody.position.y 
 
-	if (keyDown(DOWN_ARROW)) {
-
-		Matter.Body.setStatic(packageBody , true); 
-	}
-
+  keyPressed();
   drawSprites();
  
 }
 
 function keyPressed() {
  if (keyCode === DOWN_ARROW) {
-    // Look at the hints in the document and understand how to make the package body fall only on
+	Matter.Body.setStatic(packageBody , false); 
     
   }
 }
